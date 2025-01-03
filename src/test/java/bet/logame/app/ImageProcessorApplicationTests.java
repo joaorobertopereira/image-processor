b95e73383c609aeb118511c3286651bf8ef01004
João Roberto de França Pereira <joaorobertof@gmail.com>
2025-01-03 11:31:27 -0300

    wip: projeto base

---
+++ src/test/java/bet/logame/app/ImageProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package bet.logame.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
